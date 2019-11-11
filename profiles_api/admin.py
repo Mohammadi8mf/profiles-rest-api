9e2aa76acf28ba7a029f0afd86b8f965eac84dc6
Mohammad <mohammad74mf@gmail.com>
2019-11-11 17:20:36 +0330

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -3,5 +3,6 @@ from profiles_api import models
 
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 # Register your models here.
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -55,3 +55,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Retrive string representation of our user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as string"""
+        return self.status_text
